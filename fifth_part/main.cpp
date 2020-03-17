90c9c824c0abeaa4f094496c84ed9e9cd1f9d886
Andrei <lytovandrei@gmail.com>
2020-03-17 22:25:20 +0300

    update

---
+++ fifth_part/main.cpp
@@ -0,0 +1,9 @@
+inline
+int sum(int a, int b){
+    return a + b;
+}
+
+int main(){
+    int result = sum(1, 2);
+    return result * 2;
+}
---
+++ fifth_part/sum.cpp
@@ -0,0 +1,8 @@
+inline
+int sum(int a, int b){
+    return a + b;
+}
+
+int calc(){
+    return sum(2, 3);
+}
